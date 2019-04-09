596c689ba4c2bbcc712775a62fb32d900c7d1348
lukedjacobsen <lukedjacobsen@gmail.com>
2019-04-09 13:58:33 -0500

    Added WNBAPerGameStatistic.R with a test

---
+++ R/WNBAPerGameStatistics.R
@@ -0,0 +1,17 @@
+#' WNBA Player Statistics For a Given Season
+#'
+#' @param season A numeric year
+#' @return An object of class tbl_df containing all player statistics for each player from a WNBA season on basketball-reference.com.
+#'
+#'@examples
+#' WNBAPerGameStatistics(2018)
+
+
+
+WNBAPerGameStatistics <- function(season = 2018){
+  wnba_url <- paste("https://www.basketball-reference.com/wnba/years/",
+                    season, "_totals.html", sep = "")
+  pg <- xml2::read_html(wnba_url)
+  wnba_stats <- dplyr::tbl_df(rvest::html_table(pg, fill = T)[[1]])
+  return(wnba_stats)
+}
--- tests/testthat/test.R
+++ tests/testthat/test.R
@@ -3,3 +3,7 @@ library(WNBAballr)
 test_that('WNBAPerGameAdvStatistics works',{
   expect_equal(dim(WNBAPerGameAdvStatistics(2018))[1], 173)
 })
+
+test_that('WNBAPerGameStatistics works',{
+  expect_equal(dim(WNBAPerGameStatistics(2017))[1], 190)
+})
